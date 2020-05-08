1628a4a37db8ea510121c453ffbd52d054f676da
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-08 17:37:37 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,12 @@
 class HauntedHouses 
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name 
+      t.string :location 
+      t.string :theme 
+      t.integer :price 
+      t.boolean :family_friendly
+      t.
     end
     
   end 
