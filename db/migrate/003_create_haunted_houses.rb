0d1540dc7dd704a40a2990f84382e3b70f6d8d22
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-08 17:44:37 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class HauntedHouses
       t.string :name 
       t.string :location 
       t.string :theme 
-      t.integer :price 
+      t.float :price 
       t.boolean :family_friendly
       t.datetime :opening_date 
       t.datetime :closing_date
