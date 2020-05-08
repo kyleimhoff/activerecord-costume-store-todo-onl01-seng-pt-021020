a4a7449cd52f1eac1559f080dcda12a966e9e160
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-08 17:24:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,9 @@
 # Create your costume_stores migration here
 class CostumeStores 
   def change 
-    create_table :costume_stores do
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.
   end 
 end 
