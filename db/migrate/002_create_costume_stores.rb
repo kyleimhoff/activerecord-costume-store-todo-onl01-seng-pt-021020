192e4307a816135104593d7069895391039510e0
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-08 17:27:57 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,10 @@ class CostumeStores
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.
+      t.integer :costume_inventory 
+      t.integer :employees 
+      t.boolean :in_business? 
+      t.timestamp :opening_time
+      t.timestamp :closing_time
   end 
 end 
