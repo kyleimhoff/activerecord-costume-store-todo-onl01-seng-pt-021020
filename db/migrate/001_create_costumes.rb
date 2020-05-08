ef201646147c838708807b8aa06250f63b34c505
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-08 17:45:20 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,8 +14,7 @@ class Costumes < ActiveRecord:Migration[5.2]
       t.float :price
       t.integer :size 
       t.string :image_url
-      t.timestamp :created_at 
-      t.timestamp :updated_at 
+      t.timestamps
     end
   end 
 end 
